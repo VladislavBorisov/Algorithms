c921acac6973000bbac91fe4fb7d6df199840703
Vladislav.Borisov <Vladislav.Borisov@LTBPSOF162.bpsf.bulpros.com>
2017-04-25 17:59:23 +0300

    DirectoryTraverserBFS

---
+++ src/com/algorithms/examples/DirectoryTraverserBFS.java
@@ -0,0 +1,35 @@
+package com.algorithms.examples;
+
+import java.io.File;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Sample class, which traverses given directory based on the Breadth-First-Search (BFS) algorithm.
+ */
+public class DirectoryTraverserBFS {
+    /**
+     * Traverses and prints given directory with BFS.
+     * @param startDir - the path to the directory which should be traversed.
+     */
+	public static void traverseDir(String startDirectory) {
+		Queue<File> visitedDirsQueue = new LinkedList<File>();
+		visitedDirsQueue.add(new File(startDirectory));
+		while (visitedDirsQueue.size() > 0) {
+			File currentDir = visitedDirsQueue.remove();
+			System.out.println(currentDir.getAbsolutePath());
+			File[] children = currentDir.listFiles();
+			if (children != null) {
+				for (File child : children) {
+					if (child.isDirectory()) {
+						visitedDirsQueue.add(child);
+					}
+				}
+			}
+		}
+	}
+ 
+	public static void main(String[] args) {
+		traverseDir("Z:\\");
+	}
+}
